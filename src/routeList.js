d678277a0e491999f97e6e007093466d51fc1327
johnerry <fashanutosin7@gmail.com>
2020-06-08 20:38:34 +0100

    Added History

--- src/routeList.js
+++ src/routeList.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Order from './views/Order'
 import Community from './views/Community'
+import History from './views/History'
 import Edit from './views/Community/Editor'
 import View from './views/Community/View'
 import Page404 from './views/Pages/Page404'
@@ -10,6 +11,7 @@ import Page404 from './views/Pages/Page404'
 const routes = [
   { path: '/dashboard', exact: true, name: 'Dashboard', component: Order },
   { path: '/community', exact: true, name: 'Community', component: Community },
+  { path: '/history', exact: true, name: 'Community', component: History },
   { path: '/community/edit', exact: true, name: 'Edit', component: Edit },
   { path: '/community/edit/:id', exact: true, name: 'Edit', component: Edit },
   { path: '/community/:id', exact: true, name: 'View', component: View },
