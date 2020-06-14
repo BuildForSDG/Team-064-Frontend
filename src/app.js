e3ab13ca4b228bf75dafcdc1b9bbe6c79742f2cb
johnerry <fashanutosin7@gmail.com>
2020-06-14 01:59:43 +0100

    Changed the location of theme

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import Providers from './providers';
 import './fontawesome/font_awesome.css'
 // import '../node_modules/font-awesome/css/font-awesome.min.css'
 import './scss/style.scss';
+import './theming.css'
 
 // // NOTE: Do not adjust this file. Contact the team before making changes to this file.
 const App = () => <Providers />;
